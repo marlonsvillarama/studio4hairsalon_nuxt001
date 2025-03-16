6e2b64d64f717b6679e39b8c828f2e017ffa27f6
Marlon Villarama <marlonsvillarama@gmail.com>
2025-03-16 20:17:44 +1300

    Init assets

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
+  css: [
+    '~/assets/css/fonts.css',
+    '~/assets/css/root.css',
+    '~/assets/css/global.css'
+  ],
   devtools: { enabled: true },
-  modules: ['@nuxt/fonts', '@nuxt/icon', '@nuxt/image', '@nuxt/ui']
+  modules: ['@nuxt/fonts', '@nuxt/icon', '@nuxt/image', '@nuxt/ui'],
+  runtimeConfig: {}
 })
